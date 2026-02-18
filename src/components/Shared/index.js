dc720b6281cb9f499f6741e27ce4de6eb0800a18
isackelling <isac@shermanbuildings.com>
2026-02-18 10:54:49 -0600

    clean up

--- src/components/Shared/index.js
+++ src/components/Shared/index.js
@@ -3,8 +3,6 @@
  */
 
 export { default as ErrorBoundary } from './ErrorBoundary.jsx';
-export { default as Header } from './Header.jsx';
-export { default as Button } from './Button.jsx';
 export { default as ExpandableNoteSection } from './ExpandableNoteSection.jsx';
 export { default as PierDiagram } from './PierDiagram.jsx';
 export * from './styles.js';
